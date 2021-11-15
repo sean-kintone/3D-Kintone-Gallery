fc24961c23d0d3f9978d84d6c3e2d9c271109526
ahandsel <ahandsel@gmail.com>
2021-11-15 18:14:57 +0900

    Simple search form in React by asimdahall - Test out Webpack Setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,45 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+// https://dev.to/asimdahall/simple-search-form-in-react-using-hooks-42pg
 
-kintone.events.on('app.record.detail.show', event => {
-    alert('Hello, webpack');
-    return event;
-});
+import React from "react";
+import ReactDOM from "react-dom";
+
+const people = [
+  "Siri",
+  "Alexa",
+  "Google",
+  "Facebook",
+  "Twitter",
+  "Linkedin",
+  "Sinkedin"
+];
+function App() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([]);
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+  };
+  React.useEffect(() => {
+    const results = people.filter(person =>
+      person.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <ul>
+        {searchResults.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
