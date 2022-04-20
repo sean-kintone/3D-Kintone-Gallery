8a0eb9d974dc2c7c075f215600dbdf4281b6b699
sean-kintone <seanluse@cybozu.onmicrosoft.com>
2022-04-20 18:11:18 +0900

    Fix old comment

--- src/requests/getRecords.js
+++ src/requests/getRecords.js
@@ -1,7 +1,7 @@
 // getRecords.js - Fetches Kintone records, transforms response, & returns array of objects per record
 /**
  * Notes on Kintone responses:
- * record.x/y/z.value = value of the x, y, z coordinate fields
+ * record.length/width/depth.value = value of the length, width, depth fields
  * record.shapeType.value = the type of shape we will be using
  * record.$id.value = value of the Record number field (unique key for the record)
  */
